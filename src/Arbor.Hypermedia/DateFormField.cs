220cb776bcd893c6d043dd25cc7253dd17c4db59
Niklas Lundberg <info@nlundberg.se>
2022-04-28 21:47:22 +0200

    Updated packages.

--- src/Arbor.Hypermedia/DateFormField.cs
+++ src/Arbor.Hypermedia/DateFormField.cs
@@ -5,7 +5,7 @@ namespace Arbor.Hypermedia
 {
     public class DateFormField : HyperMediaFormField
     {
-        public DateFormField([NotNull] string name, Date? defaultValue = default) : base(name) =>
+        public DateFormField(string name, Date? defaultValue = default) : base(name) =>
             DefaultValue = defaultValue;
 
         public Date? DefaultValue { get; }
